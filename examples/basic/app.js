513ae4ca020e4ee7c5c90ca896dd4636e254e05a
James Long <longster@gmail.com>
2015-11-18 00:10:32 -0500

    add a basic example

---
+++ examples/basic/app.js
@@ -0,0 +1,30 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const { createStore, combineReducers } = require('redux');
+const { Provider } = require('react-redux');
+const { Router, Route } = require('react-router');
+const createHistory = require('history/lib/createHashHistory');
+const { syncReduxAndRouter, routeReducer } = require('redux-simple-router');
+
+const reducers = require('./reducers');
+const { App, Foo, Bar } = require('./components');
+
+const reducer = combineReducers(Object.assign({}, reducers, {
+  routing: routeReducer
+}));
+const store = createStore(reducer);
+const history = createHistory();
+
+syncReduxAndRouter(history, store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <Route path="foo" component={Foo}/>
+        <Route path="bar" component={Bar}/>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('mount')
+);
---
+++ examples/basic/components/index.js
@@ -0,0 +1,5 @@
+const App = require('./App');
+const Foo = require('./Foo');
+const Bar = require('./Bar');
+
+module.exports = { App, Foo, Bar };
