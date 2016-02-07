430820850420240fc97754553ee78baae2dd21ad
Tim Dorr <timdorr@timdorr.com>
2016-02-07 02:18:50 -0500

    Add back some other relevant tests.

--- test/_createSyncTest.js
+++ test/_createSyncTest.js
@@ -1,12 +1,10 @@
 import expect from 'expect'
 
 import { createStore, combineReducers } from 'redux'
-// import { ActionCreators, instrument } from 'redux-devtools'
-// import { useBasename, useQueries } from 'history'
+import { ActionCreators, instrument } from 'redux-devtools'
 
 import syncHistoryWithStore from '../src/sync'
 import { routerReducer } from '../src/reducer'
-// import routerMiddleware from '../src/middleware'
 
 expect.extend({
   toContainLocation({
@@ -42,7 +40,7 @@ function createSyncedHistoryAndStore(testHistory) {
 
 const defaultReset = () => {}
 
-module.exports = function createTests(testHistory, name, reset = defaultReset) {
+export default function createTests(testHistory, name, reset = defaultReset) {
   describe(name, () => {
 
     beforeEach(reset)
@@ -131,7 +129,91 @@ module.exports = function createTests(testHistory, name, reset = defaultReset) {
           action: 'REPLACE'
         })
       })
+
+      it('provices an unsubscribe method to stop listening to history and store', () => {
+        history.push('/foo')
+        expect(store).toContainLocation({
+          pathname: '/foo'
+        })
+
+        history.unsubscribe()
+
+        history.push('/bar')
+        expect(store).toContainLocation({
+          pathname: '/foo'
+        })
+      })
+
+      it('updates the router even if path is the same', () => {
+        history.push('/')
+
+        const updates = []
+        const historyUnsubscribe = history.listen(location => {
+          updates.push(location.pathname)
+        })
+
+        history.push('/foo')
+        history.push('/foo')
+        history.replace('/foo')
+
+        expect(updates).toEqual([ '/', '/foo', '/foo', '/foo' ])
+
+        historyUnsubscribe()
+      })
     })
 
+    describe('Redux DevTools', () => {
+      let history, store, devToolsStore
+
+      beforeEach(() => {
+        // Set initial URL before syncing
+        testHistory.push('/foo')
+
+        store = createStore(
+          combineReducers({
+            routing: routerReducer
+          }),
+          instrument()
+        )
+        devToolsStore = store.liftedStore
+
+        history = syncHistoryWithStore(testHistory, store)
+      })
+
+      afterEach(() => {
+        history.unsubscribe()
+      })
+
+      it('resets to the initial url', () => {
+        let currentPath
+        const historyUnsubscribe = history.listen(location => {
+          currentPath = location.pathname
+        })
+
+        history.push('/bar')
+        devToolsStore.dispatch(ActionCreators.reset())
+
+        expect(currentPath).toEqual('/foo')
+
+        historyUnsubscribe()
+      })
+
+      it('handles toggle after history change', () => {
+        let currentPath
+        const historyUnsubscribe = history.listen(location => {
+          currentPath = location.pathname
+        })
+
+        history.push('/foo2') // DevTools action #2
+        history.push('/foo3') // DevTools action #3
+
+        // When we toggle an action, the devtools will revert the action
+        // and we therefore expect the history to update to the previous path
+        devToolsStore.dispatch(ActionCreators.toggleAction(3))
+        expect(currentPath).toEqual('/foo2')
+
+        historyUnsubscribe()
+      })
+    })
   })
 }
