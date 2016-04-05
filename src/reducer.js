d24f9d38cf5adffd41ea6c9bed75c63cab74f051
Ken Carpenter <ken@mindjuice.net>
2016-04-05 15:43:41 -0700

    Fixes #349
    Fixes #349 in parent
    
    Also fixed a typo in the function comment.

--- src/reducer.js
+++ src/reducer.js
@@ -12,9 +12,9 @@ const initialState = {
  * This reducer will update the state with the most recent location history
  * has transitioned to. This may not be in sync with the router, particularly
  * if you have asynchronously-loaded routes, so reading from and relying on
- * this state it is discouraged.
+ * this state is discouraged.
  */
-export function routerReducer(state = initialState, { type, payload }) {
+export function routerReducer(state = initialState, { type, payload } = {}) {
   if (type === LOCATION_CHANGE) {
     return { ...state, locationBeforeTransitions: payload }
   }
