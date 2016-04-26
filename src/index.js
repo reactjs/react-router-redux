ca0bcb769243d2964a93474f6ffb121509d8e660
scalder <scalder@skbkontur.ru>
2016-04-26 18:13:40 +0500

    fix ie8 compatibility
    babel export-from doesn't work well with ie8 and ie8 babel plugins

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-export syncHistoryWithStore from './sync'
-export { LOCATION_CHANGE, routerReducer } from './reducer'
+import syncHistoryWithStore from './sync'
+import { LOCATION_CHANGE, routerReducer } from './reducer'
 
-export {
+import {
   CALL_HISTORY_METHOD,
   push, replace, go, goBack, goForward,
   routerActions
 } from './actions'
-export routerMiddleware from './middleware'
+import routerMiddleware from './middleware'
+
+export {
+    syncHistoryWithStore,
+    LOCATION_CHANGE,
+    routerReducer,
+    CALL_HISTORY_METHOD,
+    push,
+    replace,
+    go,
+    goBack,
+    goForward,
+    routerActions,
+    routerMiddleware
+}
