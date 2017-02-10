970b0d17bcda51c8f53892cae434c3736f31b22d
Tim Dorr <timdorr@timdorr.com>
2017-02-10 10:56:16 -0500

    Upgrade webpack to 2 and other deps too

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,21 @@ var webpack = require('webpack')
 var config = {
   entry: './src/index',
   module: {
-    loaders: [
-      { test: /\.js$/, loaders: [ 'babel' ], exclude: /node_modules/ }
+    rules: [
+      { test: /\.js$/, use: [ 'babel-loader' ], exclude: /node_modules/ }
     ]
   },
   output: {
     library: 'ReactRouterRedux',
     libraryTarget: 'umd'
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin()
-  ]
+  }
 }
 
 if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
-  )
+  config.plugins = [
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+    new webpack.LoaderOptionsPlugin({ minimize: true })
+  ]
 }
 
 module.exports = config
